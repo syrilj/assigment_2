2518c410660cb93a6bdc217b2c9e47a350511926
Syril Jacob <syriljacob@Syrils-MacBook-Pro.local>
2023-02-11 17:08:09 -0700

    changes

--- ex2.2.py
+++ ex2.2.py
@@ -27,7 +27,7 @@ def func2(array, start, end):
     return high
 
 # Read data from the JSON file
-with open("ex2.json", "r") as f:
+with open("ex2.5.json", "r") as f:
     arr_raw = json.load(f)
 arr = []
 for element in arr_raw:
@@ -45,25 +45,22 @@ start_time = time.time()
 func1(arr, low, high)
 end_time = time.time()
 
-# Print the sorted array
-print(arr)
-
 # Plot the timing results
-plt.plot([start_time, end_time], [0, 0], marker="o")
+time_points = [start_time, end_time]
+execution_time = [0, end_time - start_time]
+plt.plot(time_points, execution_time)
 plt.xlabel("Time (seconds)")
 plt.ylabel("Execution Time")
 plt.title("Quick Sort Timing Results")
 plt.show()
+
 print(end_time-start_time)
+#import random 
+# Partially sort the data by randomly swapping elements
+#for i in range(len(arr_raw) // 2):
+ #   j = random.randint(i, len(arr_raw) - 1)
+  #  arr_raw[i], arr_raw[j] = arr_raw[j], arr_raw[i]
 
-# Call func1 to sort the array
-#func1(arr, low, high)
-
-# Print the sorted array
-#print(arr)
-
-# Plot the sorted array
-#plt.plot(arr)
-#plt.show()
-
-
+# Save the partially sorted arr_raw to a new JSON file
+#with open("ex2.5.json", "w") as file:
+#    json.dump(arr_raw, file)
