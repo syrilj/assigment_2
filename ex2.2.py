90ce7c040693f61bd3ed5248f269e49b6fe948dc
Syril Jacob <syriljacob@Syrils-MacBook-Pro.local>
2023-02-12 13:08:42 -0700

    changes

--- ex2.2.py
+++ ex2.2.py
@@ -29,32 +29,10 @@ def func2(array, start, end):
 # Read data from the JSON file
 with open("ex2.5.json", "r") as f:
     arr_raw = json.load(f)
-arr = []
-for element in arr_raw:
-    if isinstance(element, list):
-        # If the element is a list, convert its elements to numbers
-        arr.extend([int(x) for x in element])
-    else:
-        # If the element is not a list, convert it to a number
-        arr.append(int(element))
 
-low = 0
-high = len(arr) - 1
 
-start_time = time.time()
-func1(arr, low, high)
-end_time = time.time()
 
-# Plot the timing results
-time_points = [start_time, end_time]
-execution_time = [0, end_time - start_time]
-plt.plot(time_points, execution_time)
-plt.xlabel("Time (seconds)")
-plt.ylabel("Execution Time")
-plt.title("Quick Sort Timing Results")
-plt.show()
 
-print(end_time-start_time)
 #import random 
 # Partially sort the data by randomly swapping elements
 #for i in range(len(arr_raw) // 2):
@@ -64,3 +42,20 @@ print(end_time-start_time)
 # Save the partially sorted arr_raw to a new JSON file
 #with open("ex2.5.json", "w") as file:
 #    json.dump(arr_raw, file)
+
+plot_res = []
+
+for n in arr_raw:
+   start_time = time.time()
+   func1(n, 0, len(n) - 1)
+   end_time = time.time()
+   elapsed_time = end_time - start_time
+   plot_res.append(elapsed_time)
+
+plt.plot(plot_res)
+plt.xlabel("Array Number")
+plt.ylabel("Execution Time(seconds)")
+plt.title("Quick Sort Timing Results")
+plt.show()
+
+print(elapsed_time)
